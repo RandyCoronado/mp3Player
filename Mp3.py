82a86ed3ed0a1890448a1fb35609367c4feaf8b3
DavidM54705 <davidmartinez54705@gmail.com>
2026-04-24 21:09:00 -0700

    Created beginning structure.

---
+++ Mp3.py
@@ -0,0 +1,33 @@
+
+class Running:
+    def __init__(self, root):
+        
+        
+    def load_music(self):
+        
+
+    def play_music(self):
+        
+
+    def updateTimer(self):
+        
+
+    def pause_music(self):
+        
+
+    def unpause_music(self):
+        
+
+    def queue_music(self):
+        
+
+    def next_song(self):
+       
+
+    def fetch_album_art(self, song_query):
+       
+
+    def check_end(self):
+
+
+if __name__ == "__main__":
